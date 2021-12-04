6643426700e4da0d2086a2dbc1b4b1087632f9ee
Ken Crawford <KenCrawford@Fastmail.com>
2021-12-04 13:53:46 -0500

    Project complete - but hope to improve Vimeo video

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Profile-Generator
+# Profile Generator
+## Description
+Creates web page display of employee profiles.
+## Table of Contents
+- [Installation Instructions](#installation-instructions)
+- [Usage Information](#usage-information)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Questions](#questions)
+## Installation Instructions
+All directories and files are found in the Githum Profile-Generator.
+## Usage Information
+Watch this how-to-use Vimeo-hosted video: https://vimeo.com/653244487
+## Contribution Guidelines
+N/A
+## Test Instructions
+N/A
+## License
+[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+## Questions
+### Github account: www.github.com/kencford
+### Email: 
---
+++ index.js
@@ -0,0 +1,294 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+//
+const render = require("./src/page-template.js");
+
+const teamMembers = [];
+const idArray = [];
+
+function appMenu() {
+
+  function createManager() {
+    console.log("Please build your team");
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "managerName",
+        message: "What is the team manager's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerId",
+        message: "What is the manager's id?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerEmail",
+        message: "What is the team manager's email?",
+        validate: answer => {
+          const pass = answer.match(
+            /\S+@\S+\.\S+/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        }
+      },
+      {
+        type: "input",
+        name: "managerOfficeNumber",
+        message: "What is the team manager's office number?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      }
+    ]).then(answers => {
+
+      // TODO: YOUR CODE HERE
+      // create a manager object from class manager
+
+      const manager = new Manager(
+        answers.managerName,
+        answers.managerId,
+        answers.managerEmail,
+        answers.managerOfficeNumber
+      );
+
+      // TODO: YOUR CODE HERE
+      // add the manager object to teamMembers
+      teamMembers.push(manager);
+      // console.log(teamMembers);
+      // TODO: YOUR CODE HERE
+      // add manager id to idArray
+      idArray.push(manager.getId());
+      // console.log('idArray: ', idArray);
+
+      createTeam();
+    });
+  }
+
+  function createTeam() {
+    // console.log("in createTeam");
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "memberChoice",
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members"
+        ]
+      }
+    ]).then(userChoice => {
+      switch (userChoice.memberChoice) {
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+        default:
+          // console.log(teamMembers);
+          // console.log('idArray: ', idArray);
+          buildTeam();
+      }
+    });
+  }
+
+  function addEngineer() {
+    inquirer.prompt([
+      // TODO: YOUR CODE HERE
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the engineer's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's id?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the team engineer's email?",
+        validate: answer => {
+          const pass = answer.match(
+            /\S+@\S+\.\S+/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        }
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the team engineer's github account?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      }
+
+    ]).then(answers => {
+      // TODO: YOUR CODE HERE
+      // create an engineer object from class Engineer
+      const engineer = new Engineer(
+        answers.engineerName,
+        answers.engineerId,
+        answers.engineerEmail,
+        answers.github
+      );
+
+      // add the engineer object to teamMembers
+      teamMembers.push(engineer);
+      // console.log(teamMembers);
+
+      // TODO: YOUR CODE HERE
+      // add engineer id to idArray
+      idArray.push(engineer.getId());
+      // console.log('idArray: ', idArray);
+
+      createTeam();
+    });
+  }
+
+  function addIntern() {
+    inquirer.prompt([
+      // TODO: YOUR CODE HERE
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is the intern's id?",
+        validate: answer => {
+          const pass = answer.match(
+            /^[1-9]\d*$/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a positive number greater than zero.";
+        }
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is the team intern's email?",
+        validate: answer => {
+          const pass = answer.match(
+            /\S+@\S+\.\S+/
+          );
+          if (pass) {
+            return true;
+          }
+          return "Please enter a valid email address.";
+        }
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What is the team intern's school?",
+        validate: answer => {
+          if (answer !== "") {
+            return true;
+          }
+          return "Please enter at least one character.";
+        }
+      }
+
+    ]).then(answers => {
+      // TODO: YOUR CODE HERE
+      // create an intern object from class Intern
+      const intern = new Intern(
+        answers.internName,
+        answers.internId,
+        answers.internEmail,
+        answers.internSchool
+      );
+
+      // add the intern object to teamMembers
+      teamMembers.push(intern);
+      // console.log(teamMembers);
+
+      // add intern id to idArray
+      idArray.push(intern.getId());
+      // console.log('idArray: ', idArray);
+
+      createTeam();
+    });
+  }
+
+function buildTeam() {
+  // Create the output directory if the output path doesn't exist
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR)
+  }
+  fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+}
+
+createManager();
+
+}
+
+appMenu();
